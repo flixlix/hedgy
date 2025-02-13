b48f7e35f6dd475520a1bc92bc2be352d3e6d125
Luca Félix <luca.ziegler.felix@gmail.com>
2025-02-13 14:13:18 +0100

    refactor: :recycle: remove ScrollSnapper component and restore scroll-snap-type for improved scrolling behavior

--- src/features/landing/components/parallax.tsx
+++ src/features/landing/components/parallax.tsx
@@ -181,7 +181,7 @@ function StyleSheet() {
   return (
     <style jsx global>{`
       html {
-        // scroll-snap-type: y mandatory;
+        scroll-snap-type: y mandatory;
       }
 
       .progress {
